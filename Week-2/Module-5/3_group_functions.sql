fe6be3030f8f4659e7e377d1f9c758f05b7872d9
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-04 21:54:16 +0600

    Learn : Group Functon : MIN, MAX, COUNT, AVG, SUM

---
+++ Week-2/Module-5/3_group_functions.sql
@@ -0,0 +1,11 @@
+
+SELECT COUNT(*) FROM employees;
+SELECT SUM(salary) FROM employees;
+SELECT AVG(salary) FROM employees;
+
+SELECT MAX(salary) FROM employees;
+SELECT MIN(salary) FROM employees;
+SELECT count(*) FROM employees;
+
+
+
